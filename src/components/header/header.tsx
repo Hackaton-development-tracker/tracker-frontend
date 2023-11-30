c993fb62d99ea07e6a2b01ea0e9a5417f9f56aa0
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-11-30 12:54:10 +0300

    feat: update header with links

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,52 +1,64 @@
-import styles from './header.module.scss';
-import { RootState } from '../../services/redux/store';
-import { useNavigate } from 'react-router-dom';
-import { EXIT, ROUTE_LOGIN } from '../../utils/constants';
-import { logoutUser } from '../../services/redux/slices/auth/auth';
-import { useAppDispatch, useAppSelector } from '../../services/typeHooks';
-
+import { Toolbar, Box, Link, Typography } from '@mui/material';
+import { SKILLS_PROFILE, MAP } from '../../utils/constants';
 
 const Header = () => {
-  const isLoggedIn = useAppSelector(
-    (state: RootState) => state.user.isLoggedIn,
-  );
-  const mail = useAppSelector((state: RootState) => state.user.user?.email);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const access = localStorage.getItem('accessToken') ?? '';
-  const refresh = localStorage.getItem('refreshToken') ?? '';
-  
-  const handleLogout = () => {
-    dispatch(logoutUser({ access, refresh })).then((resultAction) => {
-      if (logoutUser.fulfilled.match(resultAction)) {
-        navigate(ROUTE_LOGIN);
-      } else {
-      }
-    });
-  };
-
   return (
-    <div className={styles.header}>
-      <div>
-        {isLoggedIn ? (
-          // Если пользователь залогинен, показываем кнопку "Выйти"
-          <div className={styles.profgroup}>
-            <div className={styles.profile}>
-              <div className={styles.details}>
-                {mail}
-                <br />
-                &nbsp;
-              </div>
-            </div>
-            <button className={styles.logout} onClick={handleLogout}>
-              {EXIT}
-            </button>
-          </div>
-        ) : (
-          ''
-        )}
-      </div>
-    </div>
+    <Toolbar
+      sx={{
+        backgroundColor: '#1A1B22',
+      }}
+    >
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          padding: '0 12px',
+          minHeight: '60px',
+          alignItems: 'center',
+          paddingLeft: '200px',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+        >
+          <Link
+            onClick={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: 'YS Text Regular',
+                fontSize: '14px',
+                color: '#FFFFFF',
+              }}
+            >
+              {SKILLS_PROFILE}
+            </Typography>
+          </Link>
+          <Link
+            onClick={(event) => {
+              event.preventDefault();
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: 'YS Text Regular',
+                fontSize: '14px',
+                color: '#FFFFFF',
+              }}
+            >
+              {MAP}
+            </Typography>
+          </Link>
+        </Box>
+      </Box>
+    </Toolbar>
   );
 };
 
