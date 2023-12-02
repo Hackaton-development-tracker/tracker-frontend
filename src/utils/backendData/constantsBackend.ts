a565e6537cbab6e717c18216ca7c51056647b4cc
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-12-03 00:19:43 +0300

    feat: updated backend constants

--- src/utils/backendData/constantsBackend.ts
+++ src/utils/backendData/constantsBackend.ts
@@ -1,6 +1,51 @@
 // TODO: backend - get data
 export const userTitle = 'Продакт-менеджер';
-export const userCurrentLevel = 'Джуниор+';
+export const userCurrentLevel = 'Джун+';
 export const testDate = '2023-11-30T15:00:00.000Z';
 export const nextTestDate = '2023-12-14T15:00:00.000Z';
 export const userNextLevel = 'Мидл';
+export const projectManagerLevels = [
+  {
+    id: 1,
+    title: 'Джун',
+    level: 1
+  },
+  {
+    id: 2,
+    title: 'Мидл',
+    level: 2
+  },
+  {
+    id: 3,
+    title: 'Синьор',
+    level: 3
+  },
+]
+
+export const developerLevels = [
+  {
+    id: 1,
+    title: 'Джун',
+    level: 1
+  },
+  {
+    id: 2,
+    title: 'Джун+',
+    level: 2
+  },
+  {
+    id: 3,
+    title: 'Мидл',
+    level: 3
+  },
+  {
+    id: 4,
+    title: 'Мидл+',
+    level: 4
+  },
+  {
+    id: 5,
+    title: 'Синьор',
+    level: 5
+  },
+]
