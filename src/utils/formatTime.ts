32075f76558685f52560470a1441ffdbce083777
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-12-02 12:25:57 +0300

    refactor: use date-fns to format time

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,16 +1,13 @@
+import { format, intervalToDuration } from 'date-fns';
+
 export const formatTime = (time: number) => {
-  const days = Math.floor(time / (24 * 3600));
-  const hours = Math.floor((time % (24 * 3600)) / 3600);
-  const minutes = Math.floor((time % 3600) / 60);
-  const seconds = time % 60;
+  const duration = intervalToDuration({ start: 0, end: time * 1000 });
 
-  const formattedHours = hours.toString().padStart(2, '0');
-  const formattedMinutes = minutes.toString().padStart(2, '0');
-  const formattedSeconds = seconds.toString().padStart(2, '0');
-
-  if (days === 0) {
-    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  if (duration.days === undefined || duration.days === 0) {
+    return format(new Date(time * 1000), 'HH:mm:ss');
   }
+
+  const days = Math.floor(duration.days);
 
   if (days > 1 && days < 5) {
     return `${days} дня`;
