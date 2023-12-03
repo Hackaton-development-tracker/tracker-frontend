307a2d6b04f9379bd661724490fff41baf145779
Andrey Simonov <sasmeo@gmail.com>
2023-12-03 14:59:52 +0600

    step1

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,50 +1,24 @@
 import styles from './header.module.scss';
-import { RootState } from '../../services/redux/store';
-import { useNavigate } from 'react-router-dom';
-import { EXIT, ROUTE_LOGIN } from '../../utils/constants';
-import { logoutUser } from '../../services/redux/slices/auth/auth';
-import { useAppDispatch, useAppSelector } from '../../services/typeHooks';
+// import { RootState } from '../../services/redux/store';
+// import { useNavigate } from 'react-router-dom';
+// import { EXIT, ROUTE_LOGIN } from '../../utils/constants';
+// import { logoutUser } from '../../services/redux/slices/auth/auth';
+// import { useAppDispatch, useAppSelector } from '../../services/typeHooks';
 
 
 const Header = () => {
-  const isLoggedIn = useAppSelector(
-    (state: RootState) => state.user.isLoggedIn,
-  );
-  const mail = useAppSelector((state: RootState) => state.user.user?.email);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const access = localStorage.getItem('accessToken') ?? '';
-  const refresh = localStorage.getItem('refreshToken') ?? '';
-  
-  const handleLogout = () => {
-    dispatch(logoutUser({ access, refresh })).then((resultAction) => {
-      if (logoutUser.fulfilled.match(resultAction)) {
-        navigate(ROUTE_LOGIN);
-      } else {
-      }
-    });
-  };
+  // const isLoggedIn = useAppSelector(
+  //   (state: RootState) => state.user.isLoggedIn,
+  // );
+  // const mail = useAppSelector((state: RootState) => state.user.user?.email);
+  // const dispatch = useAppDispatch();
+  // const navigate = useNavigate();
+  // const access = localStorage.getItem('accessToken') ?? '';
+  // const refresh = localStorage.getItem('refreshToken') ?? '';
 
   return (
     <div className={styles.header}>
       <div>
-        {isLoggedIn ? (
-          // Если пользователь залогинен, показываем кнопку "Выйти"
-          <div className={styles.profgroup}>
-            <div className={styles.profile}>
-              <div className={styles.details}>
-                {mail}
-                <br />
-                &nbsp;
-              </div>
-            </div>
-            <button className={styles.logout} onClick={handleLogout}>
-              {EXIT}
-            </button>
-          </div>
-        ) : (
-          ''
-        )}
       </div>
     </div>
   );
---
+++ src/components/radio.tsx
@@ -0,0 +1,12 @@
+import { Radio, styled } from '@mui/material';
+import vars from '@/static/scss/export.module.scss';
+
+export const RadioTap = styled(Radio)(() => ({
+  '&.MuiRadio-root': {
+    padding: '4px',
+    color: vars.colorBlack300,
+  },
+  '&.Mui-checked': {
+    color: vars.colorBlueMain,
+  },
+}));
