d8fe4166079d4953f5700555b9c32eea5191f9ff
Andrey Simonov <sasmeo@gmail.com>
2023-12-09 04:49:40 +0600

    Fix navigation panel

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from 'react';
-import profileImg from '../../static/assets/icons/profile.png';
+import profileImg from '../../static/assets/icons/user.svg';
 import './navigation.scss';
 import { NavLink, useLocation } from 'react-router-dom';
 import menuLinksData from '../data/navigation.json';
 import menuFooterLinksData from '../data/navigation_footer.json';
-const getIconPath = (filename: string) => `../../static/assets/icons/${filename}.svg`;
+const getIconPath = (filename: string) =>
+  `../../static/assets/icons/${filename}.svg`;
 import { RootState } from '../../services/redux/store';
 import { useAppSelector } from '../../services/typeHooks';
+import {
+  ROUTE_STEP1,
+  ROUTE_STEP1_MESSAGE,
+  ROUTE_STEP2,
+  ROUTE_STEP2_MESSAGE,
+  ROUTE_STEP3,
+  ROUTE_STEP3_MESSAGE,
+  ROUTE_TRACKER,
+  ROUTE_TRACKER_MESSAGE,
+} from '../../utils/constants';
 
 const MainMenu: React.FC = () => {
   const location = useLocation();
@@ -73,6 +84,24 @@ const SettingsMenu: React.FC = () => {
   );
 };
 
+const NextStageText: React.FC = () => {
+  const location = useLocation();
+
+  // Определите текст в зависимости от текущего пути
+  let nextStageText = '';
+  if (location.pathname === ROUTE_STEP1) {
+    nextStageText = ROUTE_STEP1_MESSAGE;
+  } else if (location.pathname === ROUTE_STEP2) {
+    nextStageText = ROUTE_STEP2_MESSAGE;
+  } else if (location.pathname === ROUTE_STEP3) {
+    nextStageText = ROUTE_STEP3_MESSAGE;
+  } else if (location.pathname === ROUTE_TRACKER) {
+    nextStageText = ROUTE_TRACKER_MESSAGE;
+  }
+
+  return <>{nextStageText}</>;
+};
+
 const Profile: React.FC = () => {
   const mail = useAppSelector((state: RootState) => state.user.user?.email);
 
@@ -85,7 +114,9 @@ const Profile: React.FC = () => {
         </div>
         <div className="profile__stage">
           <span className="profile__stage-current">Текущий этап</span>
-          <span className="profile__stage-next">Акселерация</span>
+          <span className="profile__stage-next">
+            <NextStageText />
+          </span>
         </div>
       </div>
     </div>
